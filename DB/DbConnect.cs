893276267a41194f056936d716801de6d29d5563
young chul park <p4104@naver.com>
2019-03-21 16:57:28 +0900

    777

--- DB/DbConnect.cs
+++ DB/DbConnect.cs
@@ -808,7 +808,7 @@ namespace TIIZChatBotV3.DB
                 cmd.CommandText += "  FROM TBL_DLG_RELATION_LUIS A, TBL_DLG B                                                    ";
                 cmd.CommandText += " WHERE A.DLG_ID = B.DLG_ID                                               ";
                 //cmd.CommandText += " WHERE LUIS_INTENT = @intentId                                                 ";
-                cmd.CommandText += "   AND A.LUIS_ENTITIES = @entities                                                ";
+                //cmd.CommandText += "   AND A.LUIS_ENTITIES = @entities                                                ";
                 //cmd.CommandText += "   AND LUIS_ID = @luisId                                                        ";
 
                 if (intentId != null)
